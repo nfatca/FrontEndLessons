c695c10b54e2839773b8c81d33947b2024f69eaa
Nurullah Furkan Atca <nfurkanatca@gmail.com>
2022-08-06 14:05:01 +0300

    js_session1

---
+++ JS/session1/app.js
@@ -0,0 +1,38 @@
+console.log('app.js is running');
+// alert('Hello World')
+
+// var app = express();
+
+// let firstname = prompt('Enter your name ');
+// alert('firstname' + ' ' +firstname);
+
+// let sonuc = confirm('are u legal?');
+// console.log(sonuc);
+
+// var number1= 11;
+// console.log(number1);
+// let v1=55;
+// console.log(v1);
+// const l1=333;
+// console.log(l1);
+
+
+
+// var number1= 22;
+// console.log(number1);
+//  v1=88;
+// console.log(v1);
+// l1=444;
+// console.log(l1);
+
+const student ={
+    name: 'kaan',
+    age: 31,
+};
+console.log(student);
+
+const otherStudent = student;
+
+otherStudent.age = 29;
+console.log(otherStudent);
+console.log(student);
