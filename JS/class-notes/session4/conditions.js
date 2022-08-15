8f8e394f26599b68afd107df0ca26c648ec1b184
Nurullah Furkan Atca <nfurkanatca@gmail.com>
2022-08-15 22:21:32 +0300

    conditions

---
+++ JS/class-notes/session4/conditions.js
@@ -0,0 +1,29 @@
+//* ===============================================
+//*                  KARAR YAPILARI
+//* ===============================================
+
+//? **************** IF-ELSE *******************
+
+console.log("****** CONDITIONS *******");
+
+const sayi = +prompt(`Bir sayı giriniz`);
+const sayi1 = +prompt(`Bir sayı giriniz`);
+const sayi2 = +prompt(`Bir sayı giriniz`);
+
+// if(sayi>0){
+//     console.log(`${sayi} pozitif`);
+// }
+// else if(sayi<0){
+//     console.log(`${sayi} negatif`);
+// }
+// else{console.log(`sayı 0`)};
+// console.log(sayi, typeof sayi);
+
+if (sayi>sayi1 && sayi>sayi2) {
+    console.log(`${sayi} en büyüktür`)
+    
+}
+else if (sayi1>sayi && sayi1>sayi2) {
+    console.log(`${sayi1} en büyüktür`)
+}
+else{console.log(`${sayi2} en büyüktür.`)}
