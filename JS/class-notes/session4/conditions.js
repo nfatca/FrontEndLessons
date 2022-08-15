67830bed3470ceb32cdaf1a6bcd0d87a192e143a
Nurullah Furkan Atca <nfurkanatca@gmail.com>
2022-08-15 23:12:30 +0300

    conditions

--- JS/class-notes/session4/conditions.js
+++ JS/class-notes/session4/conditions.js
@@ -6,11 +6,11 @@
 
 console.log("****** CONDITIONS *******");
 
-const sayi = +prompt(`Bir sayı giriniz`);
-const sayi1 = +prompt(`Bir sayı giriniz`);
-const sayi2 = +prompt(`Bir sayı giriniz`);
+// const sayi = +prompt(`Bir sayı giriniz`);
+// const sayi1 = +prompt(`Bir sayı giriniz`);
+// const sayi2 = +prompt(`Bir sayı giriniz`);
 
-// if(sayi>0){
+//* if(sayi>0){
 //     console.log(`${sayi} pozitif`);
 // }
 // else if(sayi<0){
@@ -19,11 +19,28 @@ const sayi2 = +prompt(`Bir sayı giriniz`);
 // else{console.log(`sayı 0`)};
 // console.log(sayi, typeof sayi);
 
-if (sayi>=sayi1 && sayi>=sayi2) {
-    console.log(`${sayi} en büyüktür`)
+//* if (sayi>=sayi1 && sayi>=sayi2) {
+//     console.log(`${sayi} en büyüktür`)
     
-}
-else if (sayi1>=sayi && sayi1>=sayi2) {
-    console.log(`${sayi1} en büyüktür`)
-}
-else{console.log(`${sayi2} en büyüktür.`)}
+// }
+// else if (sayi1>=sayi && sayi1>=sayi2) {
+//     console.log(`${sayi1} en büyüktür`)
+// }
+// else{console.log(`${sayi2} en büyüktür.`)}
+
+//? **************** SWITCH-CASE *******************
+
+//* const yas = +prompt("Yaş giriniz");
+// const cinsiyet = prompt("Cinsiyet giriniz(erkek, kadın)");
+// const saglik = prompt("Sağlık durumunuzu giriniz (sağlıklı veya sağlıksız)");
+
+// const kosul = (yas >= 20) && (cinsiyet == `erkek`) && (saglik == `sağlıklı` || `saglikli`);
+
+// if (kosul) {
+//     console.log(`Askerlik yapmalı`)
+// }
+// else console.log(`Askerlik yapamaz`);
+
+const hız = +prompt("Hızı giriniz ");
+const mesaj = hız >= 120 ? "Hızlı" : (hız>=90 ? "Normal":"Yavaş");
+console.log(mesaj);
