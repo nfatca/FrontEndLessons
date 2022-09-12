1744d8ea8a33478638bdcffbd0ea1c7e5c9a1869
Nurullah Furkan Atca <nfurkanatca@gmail.com>
2022-09-12 23:05:40 +0300

    todo finished event handler

---
+++ JS/class-notes/session14_dom_events/2-event-example/app.js
@@ -0,0 +1,47 @@
+console.log("**** app.js *******");
+
+const langInput = document.querySelector(".lang-input");
+const addBtn = document.querySelector("#add-btn");
+const deleteBtn = document.querySelector("#delete-btn");
+const langList = document.getElementById("lang-list");
+const newUl = document.createElement("ul");
+
+langList.appendChild(newUl);
+
+//? addBtn event handler
+addBtn.addEventListener("click", () => {
+  if (!langInput.value) {
+    alert("Please enter a things to do.");
+  } else {
+    newUl.innerHTML += `<li>${langInput.value}</li>`;
+    langInput.value = "";
+  }
+  langInput.focus();
+});
+
+//? deleteBtn event handler
+deleteBtn.addEventListener("click", () => {
+  !newUl.childElementCount
+    ? alert("There is no item to delete")
+    : newUl.removeChild(newUl.lastElementChild);
+  langInput.focus();
+});
+
+//? enter key and delete key event handler
+langInput.addEventListener("keydown", (event) => {
+  //   console.log(event);
+  //   console.log(event.target);
+  //   console.log(event.keyCode); //? klavyeden aldığımız tuşun keycode değerini göteriyor
+  //   console.log(event.code); //? klavyeden aldığımız tuşun keycode adını göteriyor
+
+  if (event.keyCode === 13) {
+    addBtn.click();
+  } else if (event.code === "Delete") {
+    deleteBtn.click();
+  }
+});
+
+//? onLoad event handler
+window.addEventListener("load", () => {
+  langInput.focus();
+});
