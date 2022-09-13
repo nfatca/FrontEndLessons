78c5a0de80865fbe4d7f6406acdad160c5fda602
Nurullah Furkan Atca <nfurkanatca@gmail.com>
2022-09-13 17:04:53 +0300

    dom-intro

--- JS/class-notes/session13_dom-intro/3-createNode.js
+++ JS/class-notes/session13_dom-intro/3-createNode.js
@@ -11,3 +11,18 @@ console.log(newP);
 
 const h2 = document.querySelector(".item-list h2");
 h2.after(newP);
+
+//? getAttribute()
+console.log(newP.getAttribute("class"));
+console.log(newP.getAttribute("id"));
+console.log(newP.getAttribute("name"));
+
+//? classList
+console.log(newP.classList.contains("par")); //? true
+newP.classList.add("new-class");
+console.log(newP);
+newP.classList.remove("new-class");
+
+//? INNER HTML (Ciddi Guvenlik aciklari var)
+const ul = document.querySelector("ul");
+ul.innerHTML += `<li id="list-id" class="list">Boostrap</li>`;
