1e46ccc2c7f1feec0d4d0417ceda7bd3a79e84eb
Nurullah Furkan Atca <nfurkanatca@gmail.com>
2022-09-16 23:31:27 +0300

    study

--- JS/study/study.js
+++ JS/study/study.js
@@ -1,31 +1,33 @@
 const triangle = (row) => {
-  let temp = "";
+  let temp = [];
   arr = Array.from(row);
   console.log(arr);
   for (i = 0; i < arr.length - 1; i++) {
-    if (arr[i] + arr[i + 1] == "GG") {
-      temp += "G";
-    } else if (arr[i] + arr[i + 1] == "BG" || "GB") {
-      temp += "R";
-      count++;
-    } else if (arr[i] + arr[i + 1] == "RG" || "GR") {
-      temp += "B";
-    } else if (arr[i] + arr[i + 1] == "BR" || "RB") {
-      temp += "G";
-    } else {
-      temp += arr[i];
+    if (arr[i] === arr[i + 1]) {
+      temp.push(arr[i]);
+    } else if (arr[i] === "R" && arr[i + 1] === "B") {
+      temp.push("G");
+    } else if (arr[i] === "B" && arr[i + 1] === "R") {
+      temp.push("G");
+    } else if (arr[i] === "G" && arr[i + 1] === "B") {
+      temp.push("R");
+    } else if (arr[i] === "B" && arr[i + 1] === "G") {
+      temp.push("R");
+    } else if (arr[i] === "R" && arr[i + 1] === "G") {
+      temp.push("B");
+    } else if (arr[i] === "G" && arr[i + 1] === "R") {
+      temp.push("B");
     }
-
-    console.log(temp);
+    // console.log(temp);
   }
   if (temp.length > 1) {
-    console.log(temp);
+    // console.log(temp);
     return triangle(temp);
   } else {
     return temp;
   }
 };
-console.log(triangle("RRGBRGBB"));
+console.log(triangle("RGBG"));
 
 // triangle("GB");
 // triangle("RRR");
@@ -33,3 +35,36 @@ console.log(triangle("RRGBRGBB"));
 // triangle("RBRGBRB");
 // triangle("B");
 // triangle("RBRGBRBGGRRRBGBBBGG");
+
+// let renk = "RRGBRRBRBRGBRBGGRRRBGBBBGGGRB";
+// // console.log(renk);ß
+// const hesapla = (string) => {
+//   let dizi = string.split("");
+//   let yenidizi = [];
+//   let count = 0;
+//   for (i = 0; i < dizi.length; i++) {
+//     yenidizi = [];
+//     if (dizi[i] === dizi[i + 1]) {
+//       yenidizi.push(dizi[i]);
+//     } else if (dizi[i] === "R" && dizi[i + 1] === "B") {
+//       yenidizi.push("G");
+//     } else if (dizi[i] === "B" && dizi[i + 1] === "R") {
+//       yenidizi.push("G");
+//     } else if (dizi[i] === "G" && dizi[i + 1] === "B") {
+//       yenidizi.push("R");
+//     } else if (dizi[i] === "B" && dizi[i + 1] === "G") {
+//       yenidizi.push("R");
+//     } else if (dizi[i] === "R" && dizi[i + 1] === "G") {
+//       yenidizi.push("B");
+//     } else if (dizi[i] === "G" && dizi[i + 1] === "R") {
+//       yenidizi.push("B");
+//     }
+//     console.log(i);
+//     dizi = yenidizi;
+//     console.log(yenidizi);
+//   }
+//   // return `${yenidizi}`;
+// };
+// // console.log(hesapla(renk));
+// hesapla(renk);
+// console.log(count);
