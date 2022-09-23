5d695ba057f37ee4adbbaae60a0b043d176ecdaf
Nurullah Furkan Atca <nfurkanatca@gmail.com>
2022-09-23 16:02:51 +0300

    array

---
+++ JS/study/array.js
@@ -0,0 +1,63 @@
+const sayilar = [1, 50, 2, 41, 41, 84, 8, 35, 3];
+
+const isimler = ["sezer", "ali", "hasan", "ayse"];
+
+const ogrenciler = [
+  { id: 12, isim: "sezer", yas: 30 },
+  { id: 22, isim: "hasan", yas: 15 },
+  { id: 32, isim: "fatma", yas: 55 },
+  { id: 13, isim: "nuriye", yas: 19 },
+  { id: 92, isim: "kemal", yas: 45 },
+  { id: 15, isim: "mustafa", yas: 75 },
+  { id: 44, isim: "ceren", yas: 30 },
+];
+
+// const a = isimler.shift();
+// console.log(a);
+// console.log(isimler);
+
+// const b = sayilar.pop();
+// console.log(b);
+// console.log(sayilar);
+
+// const c = isimler.unshift("Noah");
+// console.log(c);
+// console.log(isimler);
+
+// const d = isimler.push("harvey");
+// console.log(d);
+// console.log(isimler);
+
+// const e = sayilar.push(1000);
+// console.log(e);
+// console.log(sayilar);
+
+// const ogrenci1 =ogrenciler.find((ogrenci) => ogrenci.yas == 30);
+// console.log(ogrenci1);
+
+// const ogrenci2 = ogrenciler.findIndex((x) => x.isim == "kemal");
+// console.log(ogrenci2);
+
+// const isim = isimler.includes("sezer");
+// console.log(isim);
+
+// sayilar.forEach(function (e) {
+//   console.log(e);
+// });
+
+// const cift = ogrenciler.filter((x) => x.id % 2 == 0);
+// console.log(cift);
+
+// const cift = sayilar.filter((x) => x % 2 == 0);
+// console.log(cift);
+
+// const a = isimler.concat(sayilar, ogrenciler);
+// console.log(a);
+
+// const a = sayilar.map((x) => x * 3);
+// console.log(a);
+// console.log(sayilar);
+
+// console.log(isimler);
+// let a = isimler.toString();
+// console.log(a);
