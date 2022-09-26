8a80df2d6b75d2a0dd34dc78342ba54afad194f7
Nurullah Furkan Atca <nfurkanatca@gmail.com>
2022-09-27 02:30:50 +0300

    fetch done

--- JS/class-notes/session18_async1/3-fetchAPI.js
+++ JS/class-notes/session18_async1/3-fetchAPI.js
@@ -11,7 +11,27 @@
 //? bir parametre almaktadir ve bu istegin cevabini gosteren bir Promise dondurmektedir.
 
 console.log("FETCH");
-
+// let userData;
 fetch("https://api.github.com/users")
-  .then((res) => res.json())
-  .then((data) => console.log(data));
+  .then((res) => {
+    // console.log(res);
+    //! error handling
+    if (!res.ok) {
+      throw new Error("Something went wrong");
+    } else return res.json();
+  })
+  .then((data) => updateDOM(data))
+  .catch((err) => console.log(err));
+
+// console.log(userData);
+const updateDOM = (users) => {
+  const userDiv = document.querySelector(".users");
+  console.log(users);
+  users.forEach((user) => {
+    const { login, avatar_url, following_url } = user;
+    // console.log(object);
+    userDiv.innerHTML += `<h2>${login}</h2>
+    <img src="${avatar_url}" width="300px" alt="" />
+    `;
+  });
+};
