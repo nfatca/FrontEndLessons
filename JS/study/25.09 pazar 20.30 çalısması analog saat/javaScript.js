8c2067f46515b1c75a309b969020da76baecc0bf
Nurullah Furkan Atca <nfurkanatca@gmail.com>
2022-10-04 01:28:29 +0300

    analog clock added digital clock

--- JS/study/25.09 pazar 20.30 çalısması analog saat/javaScript.js
+++ JS/study/25.09 pazar 20.30 çalısması analog saat/javaScript.js
@@ -2,11 +2,15 @@ const sec = document.querySelector(".secs");
 const min = document.querySelector(".mins");
 const hours = document.querySelector(".hours");
 
+const hour = document.querySelector("#hours");
+const minute = document.querySelector("#minutes");
+const second = document.querySelector("#seconds");
+const midnight = document.querySelector("#midnight");
+
 function tiktak() {
   let second = new Date().getSeconds();
   let minute = new Date().getMinutes();
   let hour = new Date().getHours();
-
   //   console.log(hour, minute, second);
 
   sec.style.transform = `rotate(${180 + second * 6}deg)`;
@@ -15,6 +19,24 @@ function tiktak() {
 
   //   sec.style.transform = `rotate(${1}deg)`;
 }
+
+function digitalClock() {
+  hour.innerText = new Date().getHours();
+  minute.innerText = new Date().getMinutes();
+  second.innerText = new Date().getSeconds();
+  if (Number(hour.innerText) >= 0 && Number(hour.innerText) < 13) {
+    hour.innerText = "0" + hour.innerText;
+    midnight.innerText = " AM";
+  }
+  if (Number(minute.innerText) <= 9) {
+    minute.innerText = "0" + minute.innerText;
+  }
+  if (Number(second.innerText) <= 9) {
+    second.innerText = "0" + second.innerText;
+  }
+}
+
+setInterval(digitalClock, 1000);
 
 setInterval(tiktak, 1000);
 
