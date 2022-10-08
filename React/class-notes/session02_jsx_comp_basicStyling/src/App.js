947b7bb41b098aa5f6ddb33ae65c1d7e10dc1aec
Nurullah Furkan Atca <nfurkanatca@gmail.com>
2022-10-08 12:26:32 +0300

    Session02

---
+++ React/class-notes/session02_jsx_comp_basicStyling/src/App.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+//? React,JSX kullanmaktadir.
+//? JSX'de, HTMl elementleri dogrudan JS icerisinde kullanilabilir
+
+import Header from "./components/header";
+
+//! Componentler, HTML,CSS ve JS kodlarindan meydana gelmiş
+//! bir kod parcacigidir.
+
+//! Bir componentin return () kismi ise JSX kodlari icerir.
+
+//! JSX'in kendine gore bazi kurallari vardir. Ornegin bir
+//! component eger birden fazla html elementi dondurmesi
+//! gerekirse bunlari tek bir container elemani ile sarmayalarak
+//! dondurmelidir.container icin div, article, header, <> v.b
+//! herhangi bir element kullanilabilir.
+
+//? App componentinin tanimlanmasi
+function App() {
+  return <Header />;
+}
+
+export default App;
