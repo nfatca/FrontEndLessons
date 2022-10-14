fdfc65ee05402ca5d3be97450d37e9b8e127ff83
Nurullah Furkan Atca <nfurkanatca@gmail.com>
2022-10-15 02:04:13 +0300

    oncopy

--- React/class-notes/session06_other-events/src/App.js
+++ React/class-notes/session06_other-events/src/App.js
@@ -1,15 +1,16 @@
-import Form from "./components/1-forms/Form";
-import FormObject from "./components/1-forms/FormObject";
-// import KeyboardClipboard from "./components/2-keyboardClipboardEvents/KeyboardClipboard";
+// import Form from "./components/1-forms/Form";
+// import FormObject from "./components/1-forms/FormObject";
+import KeyboardClipboard from "./components/2-keyboardClipBoardEvents/KeyboardClipboard";
+
 // import MouseEvent from "./components/3-mouseEvents/MouseEvent";
 
 function App() {
   return (
     <div className="container mt-4 text-center">
       {/* <Form /> */}
-      <FormObject />
-      {/* <KeyboardClipboard />
-      <MouseEvent /> */}
+      {/* <FormObject /> */}
+      <KeyboardClipboard />
+      {/* <MouseEvent /> */}
     </div>
   );
 }
