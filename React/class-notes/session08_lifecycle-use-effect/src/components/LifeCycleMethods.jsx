26aca5d3a655ce5d2ff31a116ceb3796187b9cc1
Nurullah Furkan Atca <nfurkanatca@gmail.com>
2022-10-18 16:47:26 +0300

    LifeCycleMethod

--- React/class-notes/session08_lifecycle-use-effect/src/components/LifeCycleMethods.jsx
+++ React/class-notes/session08_lifecycle-use-effect/src/components/LifeCycleMethods.jsx
@@ -1,7 +1,7 @@
-//?=============================================================
-//?                 LIFECYCLE METOTLARI
-//?      https://reactjs.org/docs/react-component.html
-//?=============================================================
+//?==================================================================
+//?                         LIFECYCLE METOTLARI
+//?          https://reactjs.org/docs/react-component.html
+//?==================================================================
 
 //* Lifecycle metotlari componetnlerin DOM'da varoldugu sure boyunca
 //* uzerinde islem yapmamizi imkan saglayan ozel React mototlaridir.
@@ -16,10 +16,19 @@
 //* (Optional)Bir componentin guncellenmesinin sonrasi (componentDidUpdate)
 //* Bir component'in DOM agacindan kaldirilmasi sonrasi(componentWillUnmount)
 
+// 1- componentDidMount
+//! It runs only when our rendered first time. We use this for usecases like fetching data from an API or provide inital value to some state
+
+// 2- componentDidUpdate
+//! Takes two arguments prevProps, prevState. With this we say react whenever the state changes do something.
+
+// 3- componentWillUnmount
+//! This runs when we remove the component from our app or just unmounting
+
 import React from "react";
 
 class LifeCycleMethods extends React.Component {
-  //! 1-) Bir componentin olsuturulmasinda cagrilir
+  // 1-) Bir componentin olusturulmasinda cagrilir.
   constructor(props) {
     console.log("Constructor running");
     super(props);
@@ -34,27 +43,27 @@ class LifeCycleMethods extends React.Component {
     });
   };
 
-  //! 3-) Bir component DOM agacina eklendiginde calistirilir.
-  //! (İlk render sonrasi).
-  //! Her yasam dongusu icin bir kere calisir.
+  // 3-) Bir component DOM agacina eklendiginde calistirilir.
+  // (Ilk render sonrasi).
+  // Her yasam dongusu icin bir kere calisir.
   componentDidMount() {
     console.log("Mounted");
   }
 
-  //! 4-) Bu metot ilk render haric diger tüm render'lardan sonra cagrilir.
-  //!  prevState ve prevProps degerlerini parametre olarak alabilir.
+  // 4-) Bu metot ilk render haric diger tum render'lardan sonra cagriilir.
+  // prevState ve prevProps degerlerini parametre olarak alabilir.
   componentDidUpdate(prevProps, prevState) {
     console.log("Updated");
-    console.log(prevState.count);
+    // console.log(prevState.count);
   }
 
-  //! 5-) Bir component DOM agacindan kaldiriltiktan hemen sonra cagirlir.
-  //   componentWillUnmount() {
-  //     console.log("Unmounted");
-  //   }
+  // 5-) Bir component DOM agacindan kaldirildiktan hemen sonra cagrilir.
+  componentWillUnmount() {
+    console.log("Unmounted");
+  }
 
   render() {
-    //! 2-) Her bir render'da cagrilir
+    // 2-) Her bir render'da cagrilir.
     console.log("Rendered");
     return (
       <div className="container text-center">
