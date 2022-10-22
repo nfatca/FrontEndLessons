f2631a595512941de2972ab66aa6299623e6d1ba
Nurullah Furkan Atca <nfurkanatca@gmail.com>
2022-10-22 13:54:39 +0300

    goback gohome

--- React/class-notes/session11-12_router-example/rc11-12_router-example-inclass/src/pages/People.jsx
+++ React/class-notes/session11-12_router-example/rc11-12_router-example-inclass/src/pages/People.jsx
@@ -26,7 +26,7 @@ const People = () => {
               key={id}
               className=" col-sm-12 col-md-6 col-lg-4"
               type="button"
-              onClick={() => navigate(`/people/${id}`)}
+              onClick={() => navigate(`/people/${id}`, { state: person })}
             >
               <img className="rounded" src={avatar} alt="img" />
               <h6>
--- React/class-notes/session11-12_router-example/rc11-12_router-example-inclass/src/pages/PersonDetail.jsx
+++ React/class-notes/session11-12_router-example/rc11-12_router-example-inclass/src/pages/PersonDetail.jsx
@@ -1,9 +1,29 @@
-import { useParams } from "react-router-dom";
+import { useParams, useLocation, useNavigate } from "react-router-dom";
 
 const PersonDetail = () => {
+  const navigate = useNavigate();
   const { id } = useParams();
   console.log(id);
-  return <div>PersonDetail</div>;
+
+  const { state: person } = useLocation();
+  console.log(person);
+  return (
+    <div className="container text-center">
+      <h3>
+        {person.first_name} {person.last_name}
+      </h3>
+      <img className="rounded" src={person.avatar} alt="" />
+      <p>{person.email}</p>
+      <div>
+        <button onClick={() => navigate("/")} className="btn btn-success me-2">
+          Go Home
+        </button>
+        <button onClick={() => navigate(-1)} className="btn btn-warning">
+          Go Back
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default PersonDetail;
