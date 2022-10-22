6ad36460c1cb945a0c46c3350bc4f09d30c47c3b
Nurullah Furkan Atca <nfurkanatca@gmail.com>
2022-10-22 15:02:25 +0300

    done

--- React/class-notes/session11-12_router-example/rc11-12_router-example-inclass/src/pages/PersonDetail.jsx
+++ React/class-notes/session11-12_router-example/rc11-12_router-example-inclass/src/pages/PersonDetail.jsx
@@ -1,12 +1,29 @@
-import { useParams, useLocation, useNavigate } from "react-router-dom";
+import {
+  useParams,
+  useLocation,
+  useNavigate,
+  useState,
+  useEffect,
+} from "react-router-dom";
 
 const PersonDetail = () => {
   const navigate = useNavigate();
-  const { id } = useParams();
-  console.log(id);
 
-  const { state: person } = useLocation();
-  console.log(person);
+  // const { id } = useParams();
+  // console.log(id);
+
+  // const { state: person } = useLocation();
+  // console.log(person);
+
+  const [person, setPerson] = useState("");
+
+  useEffect(() => {
+    fetch(`https://reqres.in/api/users/${id}`)
+      .then((res) => res.json())
+      .then((data) => setPerson(data.data))
+      .catch((err) => console.log(err));
+  }, []);
+
   return (
     <div className="container text-center">
       <h3>
