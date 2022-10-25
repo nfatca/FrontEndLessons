7ab0f14b8ef65b6ae4ee0aaf62cc38d9e716e0c7
Nurullah Furkan Atca <nfurkanatca@gmail.com>
2022-10-25 23:13:19 +0300

    global styled

--- React/class-notes/session13_styled-comps-example/styled-example-inclass/src/App.js
+++ React/class-notes/session13_styled-comps-example/styled-example-inclass/src/App.js
@@ -1,9 +1,24 @@
 import Header from "./components/Header";
+import { ThemeProvider } from "styled-components";
+import { GlobalStyles } from "./components/styles/Global.styled";
+
+const style = {
+  colors: {
+    header: "#fff",
+    body: "#eee",
+    footer: "#8A1C4A",
+  },
+  margins: {},
+  responsive: "768px",
+};
 
 const App = () => {
   return (
     <>
-      <Header />
+      <ThemeProvider theme={style}>
+        <GlobalStyles />
+        <Header />
+      </ThemeProvider>
     </>
   );
 };
