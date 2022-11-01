834bade8207e01069b9d66e5d537aa56e8011595
Nurullah Furkan Atca <nfurkanatca@gmail.com>
2022-11-01 18:27:16 +0300

    project start

--- React/class-notes/session14-recipe-app/recipe-app-starter/src/router/AppRouter.jsx
+++ React/class-notes/session14-recipe-app/recipe-app-starter/src/router/AppRouter.jsx
@@ -2,6 +2,10 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { GlobalStyles } from "../components/globalStyles/GlobalStyles";
 import Navbar from "./../components/nav/Navbar";
+import Home from "./../pages/home/Home";
+import Login from "../pages/login/Login";
+import About from "./../pages/about/About";
+import Detail from "./../pages/detail/Detail";
 
 const AppRouter = () => {
   return (
@@ -9,7 +13,10 @@ const AppRouter = () => {
       <GlobalStyles />
       <Navbar />
       <Routes>
-        <Route />
+        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Login />} />
+        <Route path="/" element={<About />} />
+        <Route path="/" element={<Detail />} />
       </Routes>
     </BrowserRouter>
   );
