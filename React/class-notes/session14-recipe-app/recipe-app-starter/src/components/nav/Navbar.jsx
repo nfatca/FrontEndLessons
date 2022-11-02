d3b85cdda4c13840ed5b1b76adfb9607233651c7
Nurullah Furkan Atca <nfurkanatca@gmail.com>
2022-11-02 12:15:57 +0300

    add logout

--- React/class-notes/session14-recipe-app/recipe-app-starter/src/components/nav/Navbar.jsx
+++ React/class-notes/session14-recipe-app/recipe-app-starter/src/components/nav/Navbar.jsx
@@ -21,7 +21,9 @@ const Navbar = () => {
         <MenuLink to="/">Home</MenuLink>
         <MenuLink to="about">About</MenuLink>
         <MenuLink to="register">Register</MenuLink>
-        <MenuLink to="logout">Logout</MenuLink>
+        <MenuLink to="logout" onClick={() => sessionStorage.clear()}>
+          Logout
+        </MenuLink>
       </Menu>
     </Nav>
   );
