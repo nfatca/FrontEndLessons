59323605046eb725acff0335160827896d315c1c
Nurullah Furkan Atca <nfurkanatca@gmail.com>
2022-11-08 14:33:16 +0300

    navbar

--- React/class-notes/session18-19_firebase-movie-app/firebase-movie-app-inClass/src/router/AppRouter.jsx
+++ React/class-notes/session18-19_firebase-movie-app/firebase-movie-app-inClass/src/router/AppRouter.jsx
@@ -4,10 +4,12 @@ import Login from "../pages/Login";
 import Main from "../pages/Main";
 import Register from "../pages/Register";
 import MovieDetail from "../pages/MovieDetail";
+import Navbar from "../components/Navbar";
 
 const AppRouter = () => {
   return (
     <BrowserRouter>
+      <Navbar />
       <Routes>
         <Route path="/" element={<Main />} />
         <Route path="/login" element={<Login />} />
