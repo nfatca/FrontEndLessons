12a489a8747350fb33f4ccade8f53e0ce2b7dd7d
Nurullah Furkan Atca <nfurkanatca@gmail.com>
2022-11-10 01:06:13 +0300

    movieCard

--- React/class-notes/session18-19_firebase-movie-app/firebase-movie-app-inClass/src/components/Navbar.jsx
+++ React/class-notes/session18-19_firebase-movie-app/firebase-movie-app-inClass/src/components/Navbar.jsx
@@ -36,6 +36,7 @@ const Navbar = () => {
                   style={{ height: 25, width: 25 }}
                   alt="user"
                   loading="lazy"
+                  referrerPolicy="no-referrer"
                 />
               </span>
               <ul
--- React/class-notes/session18-19_firebase-movie-app/firebase-movie-app-inClass/src/pages/Main.jsx
+++ React/class-notes/session18-19_firebase-movie-app/firebase-movie-app-inClass/src/pages/Main.jsx
@@ -1,7 +1,32 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import MovieCard from "../components/MovieCard";
+
+const API_KEY = process.env.REACT_APP_TMDB_KEY;
+const FEATURED_API = `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}`;
+const SEARCH_API = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=`;
 
 const Main = () => {
-  return <div>Main</div>;
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    getMovies(FEATURED_API);
+  }, []);
+
+  const getMovies = (API) => {
+    axios
+      .get(API)
+      .then((res) => setMovies(res.data.results))
+      .catch((error) => console.log(error));
+  };
+
+  return (
+    <div className="flex justify-center flex-wrap">
+      {movies.map((movie) => (
+        <MovieCard key={movie.id} {...movie} />
+      ))}
+    </div>
+  );
 };
 
 export default Main;
