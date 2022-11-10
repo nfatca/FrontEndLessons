14ca43fe22d98a260de2198d5c7a83a74208959a
Nurullah Furkan Atca <nfurkanatca@gmail.com>
2022-11-10 22:36:15 +0300

    counterReducer

--- React/class-notes/session21_redux-intro/redux-intro-inClass/src/redux/index.jsx
+++ React/class-notes/session21_redux-intro/redux-intro-inClass/src/redux/index.jsx
@@ -1,18 +1,28 @@
-const initialState = {
-  counter: 0,
-};
+// const initialState = {
+//   counter: 0,
+// };
 
-const reducer = (state = initialState, action) => {
-  switch (action.type) {
-    case "INCREMENT":
-      return { counter: state.counter + 1 };
-    case "DECREMENT":
-      return { counter: state.counter - 1 };
-    case "CLEAR":
-      return { counter: 0 };
-    default:
-      return state;
-  }
-};
+// const reducer = (state = initialState, action) => {
+//   switch (action.type) {
+//     case "INCREMENT":
+//       return { counter: state.counter + 1 };
+//     case "DECREMENT":
+//       return { counter: state.counter - 1 };
+//     case "CLEAR":
+//       return initialState;
+//     default:
+//       return state;
+//   }
+// };
+// export default reducer;
 
-export default reducer;
+import { combineReducers, legacy_createStore as createStore } from "redux";
+import counterReducer from "./reducers/counterReducer";
+import todoReducer from "./reducers/todoReducer";
+
+const rootReducer = combineReducers({
+  count: counterReducer,
+  todo: todoReducer,
+});
+
+export const store = createStore(rootReducer);
