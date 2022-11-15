198074863126d0e5022280ed494f061cc20b4663
Nurullah Furkan Atca <nfurkanatca@gmail.com>
2022-11-16 02:20:54 +0300

    react intro

---
+++ React/class-notes/session11_router-intro/router-intro-study/src/components/Nav.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Nav = () => {
+  return (
+    <ul>
+      <li>
+        <a href="/">Home</a>
+      </li>
+      <li>
+        <a href="/instructors">Instructors</a>
+      </li>
+      <li>
+        <a href="/contact">Contact</a>
+      </li>
+    </ul>
+  );
+};
+
+export default Nav;
