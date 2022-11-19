6442818188417352b544769a4848f7967d5fb782
Nurullah Furkan Atca <nfurkanatca@gmail.com>
2022-11-19 17:13:29 +0300

    button

--- React/class-notes/session13_styled-comps-example/styled-example-study/src/components/Header.jsx
+++ React/class-notes/session13_styled-comps-example/styled-example-study/src/components/Header.jsx
@@ -1,31 +1,12 @@
 import React from "react";
-import StyledHeader, { Logo, Nav, Image } from "./styles/Header.styled";
 import Button from "./styles/Button.styled";
-import Flex from "./styles/Flex.styled";
 
 const Header = () => {
   return (
-    <StyledHeader>
-      <Nav>
-        <Logo src="./images/logo.png" alt="" />
-        <div>
-          <Button color="#A62440">Apply Courses</Button>
-          <Button bg="#A62440">Talk to Adviser</Button>
-        </div>
-      </Nav>
-      <Flex>
-        <div>
-          <h1>The IT Career of Your Dreams Starts Here!</h1>
-          <p>
-            Clarusway is a leading international software Bootcamp. Join a micro
-            class online with other trainees and learn coding skills with a
-            highly-skilled instructor.
-          </p>
-          <Button bg="#A62440">Start your New Carier</Button>
-        </div>
-        <Image src="./images/hero.jpg" />
-      </Flex>
-    </StyledHeader>
+    <div>
+      <Button bg="#a62440">Apply Courses</Button>
+      <Button color="#a62440">Talk to Adviser</Button>
+    </div>
   );
 };
 
--- React/class-notes/session13_styled-comps-example/styled-example-study/src/components/styles/Button.styled.jsx
+++ React/class-notes/session13_styled-comps-example/styled-example-study/src/components/styles/Button.styled.jsx
@@ -1,6 +1,7 @@
 import styled from "styled-components";
 
 const Button = styled.button`
+  /* ... */
   background: ${({ bg }) => bg || "white"};
   color: ${({ color }) => color || "white"};
   border: 1px solid #a62440;
@@ -8,6 +9,7 @@ const Button = styled.button`
   box-shadow: 0 0 10px rgba(0, 0, 0, 0.15);
   padding: 1rem 1.2rem;
   font-size: 1.1rem;
+  margin-right: 0.5rem;
   margin: 2rem 0.5rem;
   cursor: pointer;
   &:hover {
