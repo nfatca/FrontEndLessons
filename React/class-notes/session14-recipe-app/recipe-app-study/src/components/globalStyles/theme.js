52795716bd30a1c1f9caffd461882cc3d78a9961
Nurullah Furkan Atca <nfurkanatca@gmail.com>
2022-11-21 16:06:59 +0300

    recipe-app stıdy

---
+++ React/class-notes/session14-recipe-app/recipe-app-study/src/components/globalStyles/theme.js
@@ -0,0 +1,12 @@
+const theme = {
+  colors: {
+    navbarBgColor: "#E1F1DD",
+    mainColor: "#00ADB5",
+    logoColor: "#393E46",
+  },
+  screens: {
+    lg: "902px",
+  },
+};
+
+export default theme;
---
+++ React/class-notes/session14-recipe-app/recipe-app-study/src/pages/login/Login.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import {
+  FormContainer,
+  Header,
+  LoginContainer,
+  StyledButton,
+  StyledForm,
+  StyledImg,
+  StyledInput,
+} from "./Login.style";
+import meal from "../../assets/meal.svg";
+import { useNavigate } from "react-router-dom";
+
+const Login = () => {
+  const navigate = useNavigate();
+
+  const userInfo = {
+    username: "admin",
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    sessionStorage.setItem("user", JSON.stringify(userInfo));
+    navigate(-1);
+  };
+
+  return (
+    <LoginContainer>
+      <FormContainer>
+        <StyledImg src={meal} />
+        <Header>{"<CW/>Recipe"}</Header>
+        <StyledForm onSubmit={handleSubmit}>
+          <StyledInput placeholder="Enter username" type="text" />
+          <StyledInput placeholder="Enter password" type="password" />
+          <StyledButton type="submit">LOGIN</StyledButton>
+        </StyledForm>
+      </FormContainer>
+    </LoginContainer>
+  );
+};
+
+export default Login;
