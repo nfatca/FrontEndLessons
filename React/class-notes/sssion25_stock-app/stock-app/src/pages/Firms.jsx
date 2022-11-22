810b09a44b6ff468edaa52bf8449b44771049577
Nurullah Furkan Atca <nfurkanatca@gmail.com>
2022-11-22 20:49:17 +0300

    axios instance

--- React/class-notes/sssion25_stock-app/stock-app/src/pages/Firms.jsx
+++ React/class-notes/sssion25_stock-app/stock-app/src/pages/Firms.jsx
@@ -1,4 +1,24 @@
+import { useEffect } from "react";
+import axios from "axios";
+import { useSelector } from "react-redux";
+
 const Firms = () => {
+  const { token } = useSelector((state) => state.auth);
+  const BASE_URL = "https://14148.fullstack.clarusway.com/";
+  const getFirms = async () => {
+    try {
+      const { data } = await axios.get(`${BASE_URL}stock/firms/`, {
+        headers: { Authorization: `Token ${token}` },
+      });
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getFirms();
+  }, []);
   return <div>Firms</div>;
 };
 
