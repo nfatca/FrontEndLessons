b5d06fde84fc897581195c0606aec5669016f9e8
Nurullah Furkan Atca <nfurkanatca@gmail.com>
2022-11-22 15:42:25 +0300

    sessionStorageClear

--- React/class-notes/session14-recipe-app/recipe-app-study/src/components/nav/Navbar.jsx
+++ React/class-notes/session14-recipe-app/recipe-app-study/src/components/nav/Navbar.jsx
@@ -18,7 +18,9 @@ const Navbar = () => {
           <MenuLink to="/">Home</MenuLink>
           <MenuLink to="about">About</MenuLink>
           <MenuLink to="register">Register</MenuLink>
-          <MenuLink to="logout">Logout</MenuLink>
+          <MenuLink to="logout" onClick={() => sessionStorage.clear()}>
+            Logout
+          </MenuLink>
         </Menu>
       </Nav>
     </>
