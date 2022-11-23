ae78c195a60023037650e09f0f756b39501ab142
Nurullah Furkan Atca <nfurkanatca@gmail.com>
2022-11-23 14:43:58 +0300

    navbar

--- React/class-notes/session22_redux-project/redux-project-study/src/components/Navbar.jsx
+++ React/class-notes/session22_redux-project/redux-project-study/src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import * as React from "react";
+// import * as React from "react";
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
@@ -8,6 +8,10 @@ import { useNavigate } from "react-router-dom";
 
 export default function Navbar() {
   const navigate = useNavigate();
+  const user = true;
+  const handleLogout = () => {
+    //TODO
+  };
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static" color="secondary">
@@ -20,8 +24,16 @@ export default function Navbar() {
           >
             Clarusway News
           </Typography>
-
-          <Button color="inherit">Login</Button>
+          {user && (
+            <Button color="inherit" onClick={handleLogout}>
+              Logout
+            </Button>
+          )}
+          {!user && (
+            <Button color="inherit" onClick={() => navigate("/")}>
+              Login
+            </Button>
+          )}
         </Toolbar>
       </AppBar>
     </Box>
