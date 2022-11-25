dee09bdf0f7cfac0420b5b369f0e5e2be61e95e1
Nurullah Furkan Atca <nfurkanatca@gmail.com>
2022-11-26 02:41:24 +0300

    persist serialize

--- React/class-notes/sssion25_stock-app/stock-app-study/src/app/store.jsx
+++ React/class-notes/sssion25_stock-app/stock-app-study/src/app/store.jsx
@@ -1,8 +1,16 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "../features/authSlice";
-import { persistStore, persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage/"; // defaults to localStorage for web
-
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
 const persistConfig = {
   key: "root",
   storage,
@@ -13,6 +21,12 @@ const store = configureStore({
   reducer: {
     auth: persistedReducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
   devTools: process.env.NODE_ENV !== "production",
 });
 
